b80640841acf2bf9c27a84ca63f514f6141d15b8
Surendharsengottuvel <surenthiranviru@gmail.com>
2023-11-04 23:13:38 -0700

    First One Hour of Learning

---
+++ LearningJS/Begining/JSworking.js
@@ -0,0 +1,62 @@
+//Dynamically typed Language
+let amount=2000;
+let sentenceis="Your bill amount is :"
+console.log(sentenceis + amount)
+
+const pi=3.14
+
+// Arithmatic Operators: +, -, *, **, /, %, ++ --,
+//     Here,
+//     ** will provide square values
+//     if you using ++ in before variable it will show sudden change ++a preincremant
+//     if you using ++ in after variable it will next  change a++ postincreament
+
+// a=10 a*2=20 a**2=10 a**3=1000
+//a=11 a%2=1 % will provide remainder value
+
+ //Assignment Operator: =, +=, -=, *=, /=, **=
+
+ a=10
+ a=a+3
+ console.log(a) //= 13
+ b=4
+ b+=4
+ console.log(b) //=8
+ c=0;
+ c=b+=4
+ console.log(c) //=12
+ A=10
+ A=A**=2
+ console.log(A)
+
+/* 5/0 = Infinity 
+0/0    = NaN
+let b (not assinged) = undefined
+        =Null
+ let b ="Naveen"
+ typeof a =String       
+
+*/
+
+//comparison Operators: == === != !=== >  < >= <=
+/*
+a='5' typeof a=string
+a*5 = 25
+a==5 ans=true (here js will check not type)
+if you want to check with variable type
+a===5 ans=false
+*/
+
+//Logical Operator : && || !
+
+//Bitwise Operators: & | ~ ^ << >> >>>
+
+//***** User Input //If you gets a value from promt it will always string value
+// let name =prompt("What is your name")
+// console.log("Hi, my dear user" , name)
+
+// let yournumber=prompt("what is the value you want to multiple with 4")
+// console.log("you final output value is", yournumber*4)
+// typeof yournumber = String
+
+//so we need to user Number(yournumber)
